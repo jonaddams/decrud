69c2df736945068528ec22881a121cb85e5ac7a4
Jon Addams <jonaddams@gmail.com>
2025-12-23 14:00:55 -0500

    Refactor code structure for improved readability and maintainability; remove redundant code blocks and optimize function calls.

--- de-api-docs/javascript-viewer-api-reference_files/anchors.js
+++
@@ -1,6 +0,0 @@
-$('h4.name').each(function () {
-  var toc = $(this);
-  (id = toc.attr('id')), (href = '#' + id), (anchor = '<a class="anchor" href="' + href + '"></a>');
-
-  toc.prepend(anchor);
-});
